efd9e15ff22c43ccbaef100ae81bdf7775b82a5c
kimchoo <34254826+kimchoo@users.noreply.github.com>
2019-05-17 19:47:38 -0400

    Update session.py

--- session.py
+++ session.py
@@ -1,20 +1,31 @@
 from facecam import *
+from sys_notifications import *
+from emotion_detection import *
 import time
 
-break_condition = False
 mood_reactions = {
     "Sad": "Have a nice day!",
     "Lonely": "I your friend.",
     "Happy": "Your on fire!!",
     "Focused": "Good work.",
     "Bored": "Read a good book...."
-    } 
+    }
 
 wait_time = 5 #In minutes
 
-while break_condition is False:
+run_condition = True
+step = 0 
 
-    time.sleep(60*wait_time)
-    face =  snap()
-    react = mood_reactions[mood_detect(face)]
+while run_condition:
+    
+    time.sleep(.1)    #(60*wait_time)
+    
+    face, grey_img =  cam_snap()
+
+    react = mood_reactions[mood_detect(face, grey_img)]
+    
     create_notification(react)
+
+    if step >= 10: run_condition = False
+
+    step += 1
