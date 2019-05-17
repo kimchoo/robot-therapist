70694fe2b9fc46cafb3220bbb4cfe60e87a43556
kimchoo <34254826+kimchoo@users.noreply.github.com>
2019-05-17 13:28:00 -0400

    Update facecam.py

--- facecam.py
+++ facecam.py
@@ -1,23 +1,17 @@
 from cv2 import *
 import numpy as np
 
-#Psuedo Code:
-#   because the open cv read() method is only called once (outside a loop). the program will only work with a single picture (hence the snap name)
-#   firstly the image is converted to grey scale to reduce the neccesary compation
-#   then the face_casacde obj is used to produce a face detector, based of the xml file.
-#   after all is done release the camera and rtn the faces seen
-
-cam = cv2.VideoCapture(0) #Define a video camera to use.
+cam = cv2.VideoCapture(0)
 
 def snap():
 
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface.xml') #Create a face detection obj
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface.xml')
 
-    ret, frame = cam.read() #Read the image from the "cam" obj
-        
+    ret, frame = cam.read() 
+    
     if ret:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Convert the image to grey-scale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5) #Apply the face detection to the grey-scale image
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         
         cam.release() #Release the camera
